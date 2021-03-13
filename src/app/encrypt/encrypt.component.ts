3d30a26a666f284cf45b6b05de77afdac7d71506
Akhil Sudhakaran <akhil.sudhakaran@microfocus.com>
2021-03-13 23:57:22 +0530

    Added encryption function

--- src/app/encrypt/encrypt.component.ts
+++ src/app/encrypt/encrypt.component.ts
@@ -2,7 +2,8 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 import { DataService } from '../services/data.service';
-import { saveAs } from 'file-saver/src/FileSaver';
+import { saveAs } from 'file-saver/dist/FileSaver';
+import { pkcs5, cipher, util } from 'node-forge';
 
 @Component({
   selector: 'app-encrypt',
@@ -37,10 +38,22 @@ export class EncryptComponent implements OnInit {
     else {
       console.log('The passwords matched');
       console.log(this.passPhrase)
-      this.blob = new Blob([this.text as any], { type: 'text' });
+      var encrypted = this.aes_encrypt(this.text, this.passPhrase);
+      this.blob = new Blob([encrypted as any], { type: 'text' });
       saveAs(this.blob, "hello.txt");
       this.blob = null;
     }
+  }
+
+  aes_encrypt(text: String, passPhrase: String): string {
+    const key = pkcs5.pbkdf2(passPhrase, 'c1f01c0bfb90453a', 1000, 16);
+    const c = cipher.createCipher('AES-CBC', key);
+    c.start({ iv: 'c1f01c0bfb90453a' });
+    c.update(util.createBuffer(text, 'utf8'));
+    c.finish();
+    var returnValue = util.encode64(c.output.getBytes());
+    console.log(returnValue);
+    return returnValue;
   }
 
   @HostListener('unloaded')
